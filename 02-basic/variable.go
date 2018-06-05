3842e8e22aae84513436b5d5f0a60af41cc9e624
Aswinda Prima Putra <aswinda.pp@gmail.com>
2018-06-06 01:06:06 +0700

    varible

---
+++ 02-basic/variable.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+    // declaration variable int
+    var a int
+    a = 5
+    fmt.Printf("%d\n", a)
+    
+    // or you can
+    b := 15
+    fmt.Printf("%d\n", b)
+
+    // Multiple var declarations may also be grouped
+    var (
+        c int
+        d int32
+    )
+    c = 1; d = 2
+    fmt.Printf("%d %d\n", c, d)
+}
