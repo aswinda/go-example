7ca87eba86c1419acb1277a5a282045a7fee2650
Aswinda Prima Putra <aswinda.pp@gmail.com>
2018-06-06 01:17:27 +0700

    const

---
+++ 02-basic/constant.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    // declaration constant
+    const a = 5
+    fmt.Printf("%d\n", a)
+    
+    // this will you error, constant can't be change
+    // a = 15
+    // fmt.Printf("%d\n", a)
+}
